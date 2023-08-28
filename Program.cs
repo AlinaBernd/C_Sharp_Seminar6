435cc3e4e033af8cbf6052a16fc8f230fb895e4b
alina bernd <a-manaeva@mail.ru>
2023-08-28 18:05:19 +0300

    Добавила решение задачи 43

--- Program.cs
+++ Program.cs
@@ -33,11 +33,37 @@ int size = Convert.ToInt32(Console.ReadLine());
 
 int[] myArray = CreateArray(size);
 int count = PositiveElements(myArray);
-Console.WriteLine(count);
+Console.WriteLine("Positive Elements: " + count);
 */
 
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// Вычисления для решения:
+// x = (b2 - b1) / (k1 - k2)
+
+
+void TheIntersectionOfTwoLines(double k1, double k2, double b1, double b2)
+{
+    if (k1 - k2 == 0 && b2 - b1 == 0)
+        Console.WriteLine("Lines match.");
+    else if (k1 - k2 == 0 && b2 - b1 != 0)
+        Console.WriteLine("Lines are parallel.");
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = x * k1 + b1;
+        Console.WriteLine($"Lines intersect at a coordinate point ({x}; {y}).");
+    }
+}
+Console.Write ("Input k1 of the first line: ");
+double unknownK1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b1 of the first line: ");
+double unknownB1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input k2 of the second line: ");
+double unknownK2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b2 of the second line: ");
+double unknownB2 = Convert.ToInt32(Console.ReadLine());
+TheIntersectionOfTwoLines(unknownK1, unknownK2, unknownB1, unknownB2);
 
 // Доп. задача
