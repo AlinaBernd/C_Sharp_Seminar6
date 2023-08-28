28d961d7b35c6f3f8ae664b3503ab14251e6c9f3
alina bernd <a-manaeva@mail.ru>
2023-08-28 15:51:23 +0300

    Добавила решение задачи 41

--- Program.cs
+++ Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+/*
+int[] CreateArray(int m)
+{
+    int[] array = new int[m];
+
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Element {i + 1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int PositiveElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Enter the size of the array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateArray(size);
+int count = PositiveElements(myArray);
+Console.WriteLine(count);
+*/
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Доп. задача
